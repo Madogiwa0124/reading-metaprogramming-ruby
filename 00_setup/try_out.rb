c9b2735dc7c3240080dcbfe889138f3cf1281626
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-02-25 23:43:29 +0900

    leaned try_out.rb

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -5,4 +5,30 @@ class TryOut
   # first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
   # upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
   # upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
+
+  def initialize(first_name, middle_name='', last_name)
+    @first_name  = first_name
+    @middle_name = middle_name
+    @last_name   = last_name
+  end
+
+  attr_writer :first_name
+
+  def full_name
+    names.join(' ')
+  end
+
+  def upcase_full_name
+    full_name.upcase
+  end
+
+  def upcase_full_name!
+    names.map(&:upcase!).join(' ')
+  end
+
+  private
+
+  def names
+    [@first_name, @middle_name, @last_name].reject(&:empty?)
+  end
 end
